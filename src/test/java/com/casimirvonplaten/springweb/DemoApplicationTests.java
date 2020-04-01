0e0a699b103f62cb00fe7a6b179c497188b9a0ac
Casimir von Platen <casimir.von.platen@hotmail.com>
2020-04-01 14:03:06 +0000

    initial

---
+++ src/test/java/com/casimirvonplaten/springweb/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casimirvonplaten.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
